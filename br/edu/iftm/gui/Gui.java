a390cc3bb83742e4b5f0612c589301de847f68a9
Thaís Vilarinho <thais.apvilarinho@hotmail.com>
2020-07-09 23:47:43 +0000

    Criado a classe Gui

---
+++ br/edu/iftm/gui/Gui.java
@@ -0,0 +1,34 @@
+package br.edu.iftm.gui;
+
+import javax.swing.*;
+
+import br.edu.iftm.acoes.MovimentoTeclado;
+
+public class Gui {
+    JFrame frame;
+    DesenharEstrutura areaMatriz;
+
+    public static int larguraFrame = 800; 
+    public static int alturaFrame = 630;
+    public static int matrizPosicaoX = 60;
+    public static int matrizPosicaoY = 30;
+
+    public void criar(){
+        frame = new JFrame("Jogo da Cobrinha");
+        frame.setSize(larguraFrame,alturaFrame);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); //centralizar
+        frame.setLayout(null); //retirar o gerenciador de layout
+        frame.setResizable(false); // não deixar maximizar
+        frame.addKeyListener(new MovimentoTeclado());
+
+        areaMatriz = new DesenharEstrutura();
+        areaMatriz.setBounds(0,0,larguraFrame,alturaFrame);
+        areaMatriz.setVisible(true);
+        frame.add(areaMatriz);
+
+        frame.requestFocus();
+        frame.setVisible(true);
+
+    }
+}
